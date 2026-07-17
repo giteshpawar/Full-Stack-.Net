d93f7cee6f8a6ff56b56424b9486071cba3c56aa
Gitesh Pawar <giteshpawar3516@gmail.com>
2026-07-17 08:22:50 +0530

    Practice 16july

---
+++ Practice/16july/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace _16july.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string course { get; set; }
+
+        public string gender { get; set; }
+
+        public int fees { get; set; }
+
+        public string quali { get; set; }
+
+    }
+}
