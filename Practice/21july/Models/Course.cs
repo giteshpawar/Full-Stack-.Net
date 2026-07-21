ce0f02a056338473c52906b05d5d3eabfb310374
Gitesh Pawar <giteshpawar3516@gmail.com>
2026-07-21 11:59:05 +0530

    Practice 21july

---
+++ Practice/21july/Models/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace _21july.Models
+{
+    public class Course
+    {
+        public string courseName;
+        public string sem;
+        public string sessionTime;
+        public string days;
+    }
+}
