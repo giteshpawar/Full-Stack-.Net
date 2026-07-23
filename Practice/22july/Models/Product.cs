1e374caf1b3866a8ee468f453dcf1eb8cd852928
Gitesh Pawar <giteshpawar3516@gmail.com>
2026-07-23 09:47:28 +0530

    Practice 22july

---
+++ Practice/22july/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _22july.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is mandatory")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price is mandatory")]
+        [Range(10, 10000, ErrorMessage = "Price must be between 10 and 10000")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Category is mandatory")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Stock is mandatory")]
+        public int Stock { get; set; }
+    }
+}
