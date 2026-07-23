499b5f0cc01fea1043671375a0ce0e1d486dc7c8
Gitesh Pawar <giteshpawar3516@gmail.com>
2026-07-23 23:02:51 +0530

    Assignment 21july

---
+++ Daily_Assignment/21july/Views/Employee/Success.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _21july.Views.Employee
+{
+    public class SuccessModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
