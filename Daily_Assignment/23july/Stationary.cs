43707711b9178f87ec4f8cfce6f9159828347869
Gitesh Pawar <giteshpawar3516@gmail.com>
2026-07-26 00:15:36 +0530

    Assignment 26july

---
+++ Daily_Assignment/23july/Stationary.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _23_July.Models
+{
+    public class Stationary
+    {
+
+
+
+       
+            [Required(ErrorMessage = "Name is mandatory")]
+            public string Name { get; set; }
+
+            [Required(ErrorMessage = "Price is mandatory")]
+            [Range(10, 100000)]
+            public int Price { get; set; }
+
+            [Required(ErrorMessage = "Brand is mandatory")]
+            public string Brand { get; set; }
+
+            [Required(ErrorMessage = "Stock is mandatory")]
+            public int stock { get; set; }
+
+  
+
+}
+}
